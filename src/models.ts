08f509c96407bae54b79067ea16efde698e0a4d4
Sergey Bespalko <bespalkoser@gmail.com>
2024-01-02 00:19:23 +0200

    added time to read

--- src/models.ts
+++ src/models.ts
@@ -1,11 +1,9 @@
 export type BlogType = {
   title: string
-  content_text: string
   photo_url: string
   created_at: string
   description: string
   id: number
   content_html: string
-  category: string
   updated_at: string
 }
