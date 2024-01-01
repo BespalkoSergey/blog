5104affbf7dc515b0e4fe5377bea93b3527f7222
Sergey Bespalko <bespalkoser@gmail.com>
2024-01-01 23:09:59 +0200

    added sort to blogs

--- src/db.ts
+++ src/db.ts
@@ -10,7 +10,9 @@ export const getBlogs = async (): Promise<BlogType[]> =>
   new Promise(resolve => {
     pool.getConnection(function (_, conn) {
       conn.query('SELECT * FROM `blogs`', (_, result) => {
-        resolve(result as BlogType[])
+        const blogs = (result ?? []) as BlogType[]
+        const sortedFromNearestToLatestDate = blogs.toSorted((a, b) => (new Date(a.updated_at).getTime() - new Date(b.updated_at).getTime()) * -1)
+        resolve(sortedFromNearestToLatestDate)
       })
       pool.releaseConnection(conn)
     })
