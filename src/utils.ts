23cee48f649298cc3eb91e7f19e0141358ab53a7
Sergey Bespalko <bespalkoser@gmail.com>
2024-01-02 18:07:16 +0200

    fin layout blog

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,18 @@
+import type { BlogType } from './models'
+
 export const addZeros = (n: number): string => `00${n}`.slice(-2)
+
+export const getDate = (b: BlogType): string => {
+  const date = new Date(b.updated_at)
+  const day = date.getDate()
+  const month = date.getMonth() + 1
+  const year = date.getFullYear().toString().slice(-2)
+  return `${addZeros(day)}.${addZeros(month)}.${year}`
+}
+
+export const calculateReadingTime = (b: BlogType, averageReadingSpeed = 200, wordsPerPage = 300): number => {
+  const words = b.content_html.split(/\s+/).length
+  const minutesToRead = words / wordsPerPage / (averageReadingSpeed / 60)
+  return Math.ceil(minutesToRead)
+}
+
